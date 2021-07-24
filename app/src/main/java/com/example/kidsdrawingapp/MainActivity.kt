166dae0c108753553d3326420755fac1880f0156
maksimpa <emexpas@outlook.com>
2021-07-24 14:58:56 +0300

    added color pallete UI

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -3,16 +3,32 @@ package com.example.kidsdrawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //sets the brush size to 20
         drawing_view.setSizeForBrush(20.toFloat())
+
+        //which color should be drawn at first, [1] because first position was black in activity_main.xml
+        //im accessing ll_paint_colors which is a array list which is a linear layout that has all those image buttons, and I'm accessing the as a index position
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        //I need to tell my Image Button how it will look after I pressed it
+        mImageButtonCurrentPaint!!.setImageDrawable( //!! because it is a nullable
+            // getting the xml file
+            // once I define the color that is going to be selected, I also want it to be visible on the screen
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         //im calling the showBrushSizeChooserDialog() function
         ib_brush.setOnClickListener { showBrushSizeChooserDialog() }
